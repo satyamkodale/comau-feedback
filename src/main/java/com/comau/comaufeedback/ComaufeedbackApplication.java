d6d33fc58c0c202d073e275468c579b184122162
satyamkodale <satyamkodale@gmail.com>
2025-03-23 22:32:26 +0530

    initial project

---
+++ src/main/java/com/comau/comaufeedback/ComaufeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.comau.comaufeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComaufeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComaufeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comau/comaufeedback/ComaufeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comau.comaufeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComaufeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
